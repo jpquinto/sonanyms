0e4824e76b8a9bf5c6938b4d8c6fb501fab63504
Jeremy Quinto <jpq2901@gmail.com>
2025-10-14 11:47:58 -0700

    Added admin frontend

--- infra/word_bank_ingestion.tf
+++ infra/word_bank_ingestion.tf
@@ -1,11 +1,11 @@
-module "round_results_lambda" {
+module "word_bank_ingestion_lambda" {
   source  = "./modules/lambda"
   context = module.null_label.context
 
-  name            = "round-results-lambda"
+  name            = "word-bank-ingestion-lambda"
   handler         = "handler.handler"
-  source_dir      = "${path.root}/../lambda_functions/dist/round-results"
-  build_path      = "${path.root}/../lambda_functions/build/round-results/round-results.zip"
+  source_dir      = "${path.root}/../lambda_functions/dist/word_bank_ingestion"
+  build_path      = "${path.root}/../lambda_functions/build/word_bank_ingestion/word_bank_ingestion.zip"
   runtime         = "nodejs20.x"
   memory          = 256
   time_limit      = 60
